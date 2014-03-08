41445406f3caeddfb6d23302e5409ea359df9829
Xin Huang <xhuang@freewheel.tv>
2014-03-09 02:29:50 +0800

    rewrite the logic change to json api

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express'),
     path = require('path'),
     util = require("util"),
     Steam = require("./MatchProvider-steam").MatchProvider,
-    MongoDB = require("./MatchProvider-mongodb").MatchProvider,
     config = require("./config");
 
 var app = express(),
@@ -14,12 +13,12 @@ var app = express(),
         config.steam_guard_code,
         config.cwd,
         config.steam_response_timeout),
-    mongodb = new MongoDB(config.mongodb_host, config.mongodb_port);
+    data_cache = {};
 
 // all environments
-app.set('port', 3100);
+app.set('port', 8001);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -33,85 +32,67 @@ app.use(express.static(path.join(__dirname, 'public')));
 // }
 
 app.get('/', function(req, res){
-    res.redirect("/tools/matchurls");
+  res.send("Hello World");
 });
 
-app.get('/tools/matchurls', function(req, res){
+app.get("/api/matchurl", function(req, res) {
     var matchId = req.query.matchid;
     if (!matchId) {
-        // No match ID, display regular index.
-        res.render('index', { title: 'match urls!' });
+        res.json({ error: 'need [matchid]' });
         res.end();
     }
-    else {
-        if (!isNaN(matchId) && parseInt(matchId, 10) < 1024000000000) {
-            matchId = parseInt(matchId, 10);
 
-            mongodb.findByMatchId(matchId, function(err, data) {
-                if (err) throw err;
+    if (isNaN(matchId) || parseInt(matchId, 10) > 1024000000000) {
+        res.json({
+            error: "invalid [matchid]"
+        });
+        res.end();
+    }
 
-                if (data) {
-                    // We have this appid data already in mongodb, so just serve from there.
-                    res.render('index', {
-                        title: 'match urls!',
-                        matchid: matchId,
-                        replayState: data.state,
-                        replayUrl: util.format("http://replay%s.valve.net/570/%s_%s.dem.bz2", data.cluster, data.id, data.salt)
-                    });
-                    res.end();
-                }
-                else if (steam.ready) {
-                    // We need new data from Dota.
-                    steam.getMatchDetails(matchId, function(err, data) {
-                        if (err) {
-                            res.render('index', {
-                                title: 'match urls!',
-                                error: err
-                            });
-                            res.end();
-                        }
-                        else {
-                            // Save the new data to Mongo
-                            mongodb.save(data, function(err, cb){});
+    matchId = parseInt(matchId, 10);
+    data = data_cache[matchId];
+    if (data) {
+        res.json({
+            matchid: matchId,
+            replayState: data.state,
+            replayUrl: util.format("http://replay%s.valve.net/570/%s_%s.dem.bz2", data.cluster, data.id, data.salt)
+        });
+        res.end();
+    }
+    
+    if (!steam.ready) {
+      res.json({
+        error: "steam not ready",
+      });
+      res.end();
+    }
 
-                            res.render('index', {
-                                title: 'match urls!',
-                                matchid: matchId,
-                                replayState: data.state,
-                                replayUrl: util.format("http://replay%s.valve.net/570/%s_%s.dem.bz2", data.cluster, data.id, data.salt)
-                            });
-                            res.end();
-                        }
-                    });
-
-                    // If Dota hasn't responded by 'request_timeout' then send a timeout page.
-                    setTimeout(function(){
-                        res.render('index', {
-                            title: 'match urls!',
-                            error: "timeout"
-                        });
-                        res.end();
-                    }, config.request_timeout);
-                }
-                else {
-                    // We need new data from Dota, and Dota is not ready.
-                    res.render('index', {
-                        title: 'match urls!',
-                        error: "notready"
-                    });
-                    res.end();
-                }
-            });
-        }
-        else {
-            // Match ID failed validation.
-            res.render('index', {
-                title: 'match urls!',
-                error: "invalid"
+    steam.getMatchDetails(matchId, function(err, data) {
+        if (err) {
+            res.json({
+                error: err
             });
             res.end();
         }
-    }
+        else {
+            data_cache[matchId] = data;
+
+            res.json({
+                matchid: matchId,
+                replayState: data.state,
+                replayUrl: util.format("http://replay%s.valve.net/570/%s_%s.dem.bz2", data.cluster, data.id, data.salt)
+            });
+            res.end();
+        }
+    });
+
+    // If Dota hasn't responded by 'request_timeout' then send a timeout page.
+    setTimeout(function(){
+        res.json({
+            error: "timeout"
+        });
+        res.end();
+    }, config.request_timeout);
 });
 
 http.createServer(app).listen(app.get('port'), function(){
